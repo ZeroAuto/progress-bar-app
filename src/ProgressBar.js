fa27b94d9ccef0686894689000aba6339dc3a7da
Mike James <mikejames386@gmail.com>
2024-03-13 12:23:36 -0500

    rename some variables

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 const ProgressBar = ({activeIndex, completeEvent, idx}) => {
-  const [totalProgress, setTotalProgress] = useState(0);
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
     if (activeIndex === idx) {
       const progressInterval = setInterval(() => {
-        setTotalProgress((prevTotal) => {
+        setProgress((prevTotal) => {
           if (prevTotal < 100) {
             return prevTotal + 1;
           } else {
@@ -25,7 +25,7 @@ const ProgressBar = ({activeIndex, completeEvent, idx}) => {
     <div className="w-full bg-pink-300 h-4 rounded-full">
       <div
         className="bg-blue-400 h-full rounded-full"
-        style={{ width: `${totalProgress}%`}}
+        style={{ width: `${progress}%`}}
       />
     </div>
   );
